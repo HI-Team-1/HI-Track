5878b081c0ad8b11f590c84ea5bcf9739ae69c8f
SpikerSpiegel <alestry.perez@hyperisland.se>
2020-10-19 16:49:02 +0200

    changed some styling and, init tabs

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-const sideBoxTabs=document.querySelectorAll(".sideNavBox .sideBoxTabs button");
-const tabPanels=document.querySelectorAll(".sideNavBox .tabPanel");
+var todayButton = document.querySelector(".todayButton");
+var leftActivies = document.querySelectorAll(".leftActivies");
+var today = document.getElementById("today");
 
-function openTab(evt, tabContent) {
+todayButton.addEventListener("click", todayContent);
 
-  for (let i = 0; i < tabPanels.length; i++) {
-    tabPanels[i].style.display = "none";
+function todayContent() {
+  for (let i = 0; i < leftActivies.length; i++) {
+    leftActivies[i].style.display = "none";
   }
-  for (i = 0; i < sideBoxTabs.length; i++) {
-    sideBoxTabs[i].className = sideBoxTabs[i].className.replace(" active", "");
-  }
-  document.getElementById(tabContent).style.display = "block";
-  evt.currentTarget.className += " active";
+  today.style.display = "block";
 }
